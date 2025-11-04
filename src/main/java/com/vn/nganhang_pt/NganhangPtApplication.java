52f4db7ab4fd58f02f2ddf8209fc9c1b729f9d02
Huy Cao <ngochuy.mep@gmail.com>
2025-11-04 22:10:12 +0800

    first init

---
+++ src/main/java/com/vn/nganhang_pt/NganhangPtApplication.java
@@ -0,0 +1,13 @@
+package com.vn.nganhang_pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NganhangPtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NganhangPtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vn/nganhang_pt/NganhangPtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.nganhang_pt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NganhangPtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
